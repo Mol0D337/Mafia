fefff3fa34ddbb4ea45d1379b934f173c0f8650e
Mol0D337 <zmolodyk@gmail.com>
2020-11-25 18:55:19 +0200

    add chat validate

--- src/php/chat.vue
+++ src/php/chat.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="">
     <form @submit.prevent="lol">
-      <input type="text" class="message">
+      <input type="text" class="message" >
       <button>Send</button>
       <hr>
-      <div class="panel"></div>
+      <div class="panel" ></div>
     </form>
   </div>
 </template>
@@ -12,16 +12,31 @@
 <script>
   export default {
     name: "chat",
+
+    data () {
+      return {
+        connection: null
+      }
+    },
+
+    mounted () {
+      this.connection = new WebSocket('ws://127.0.0.1:8777');
+      console.log(this.connection, 'conn');
+    },
+
     methods : {
       lol () {
-        console.log('12124124');
         var message = document.querySelector('.message');
         var btn = document.querySelector('button');
         var panel = document.querySelector('.panel');
 
-        var conn = new WebSocket('ws://127.0.0.1:8777');
-        conn.onopen = function(e) {
+        // var conn = new WebSocket('ws://127.0.0.1:8777');
+        const conn = this.connection;
+        console.log(conn, 'ocnmn');
+        // console.log(this.connection, 'conn');
+        conn.onopen = function() {
           console.log("Connection established!");
+        //  кнопка на открытие чата
         };
         conn.onmessage = function(e) {
           console.log(e.data);
@@ -30,9 +45,11 @@
         //---------------------------
         btn.onclick = function(){
           var msg = message.value;
+          console.log(this.connection, 'aalllo');
           conn.send(msg);
+          console.log(msg);
         }
-      },
+      }
     },
   }
 </script>

d6275e1422d6aa2d249d93fc60337e0d40657d44
Mol0D337 <zmolodyk@gmail.com>
2020-12-12 11:15:41 +0200

    add infogame

--- src/views/Menu.vue
+++ src/views/Menu.vue
@@ -1,6 +1,7 @@
 <template>
     <div>
         <HeaderMainPage/>
+        <InfoGame/>
         <Footer/>
     </div>
 </template>
@@ -10,9 +11,10 @@
     import Footer from "../components/app/Footer";
     import Sign from "../components/app/Sign";
     import HeaderMainPage from "../components/app/HeaderMainPage";
+    import InfoGame from "../components/app/InfoGame";
     export default {
         name: "Menu",
-        components: {HeaderMainPage, Sign, Footer, Header}
+        components: {InfoGame, HeaderMainPage, Sign, Footer, Header}
     }
 </script>
 

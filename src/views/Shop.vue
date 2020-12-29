0165a2ed95959fc8548bf0112efbbe74893909e0
Mol0D337 <zmolodyk@gmail.com>
2020-12-29 20:40:15 +0200

    add infogame

--- src/views/Shop.vue
+++ src/views/Shop.vue
@@ -1,63 +1,63 @@
 <template>
-    <div class="bcg">
-        <Header/>
-            <div class="">
-                <ShopTabs/>
-                <BankCard
-                        :form-data="formData"
-                        @input-card-number="updateCardNumber"
-                        @input-card-name="updateCardName"
-                        @input-card-month="updateCardMonth"
-                        @input-card-year="updateCardYear"
-                        @input-card-cvv="updateCardCvv"
-                />
-            </div>
-        <Footer/>
+  <div class="bcg">
+    <Header/>
+    <div class="">
+      <ShopTabs/>
+      <BankCard
+              :form-data="formData"
+              @input-card-number="updateCardNumber"
+              @input-card-name="updateCardName"
+              @input-card-month="updateCardMonth"
+              @input-card-year="updateCardYear"
+              @input-card-cvv="updateCardCvv"
+      />
     </div>
+    <Footer/>
+  </div>
 
 </template>
 
 <script>
-    import Header from "../components/app/Header";
-    import Footer from "../components/app/Footer";
-    import ShopTabs from "../components/app/ShopTabs";
-    import CardInfo from "../components/app/CardInfo";
-    import BankCard from "../components/app/BankCard";
-    export default {
-      name: "Shop",
-      components: {BankCard, CardInfo, ShopTabs, Footer, Header},
-        data () {
-            return {
-                formData: {
-                    cardName: '',
-                    cardNumber: '',
-                    cardMonth: '',
-                    cardYear: '',
-                    cardCvv: ''
-                }
-            }
-        },
-        methods: {
-            updateCardNumber (val) {
-            },
-            updateCardName (val) {
-            },
-            updateCardMonth (val) {
-            },
-            updateCardYear (val) {
-            },
-            updateCardCvv (val) {
-            }
-        },
-    }
+  import Header from "../components/app/Header";
+  import Footer from "../components/app/Footer";
+  import ShopTabs from "../components/app/ShopTabs";
+  import CardInfo from "../components/app/CardInfo";
+  import BankCard from "../components/app/BankCard";
+  export default {
+    name: "Shop",
+    components: {BankCard, CardInfo, ShopTabs, Footer, Header},
+    data () {
+      return {
+        formData: {
+          cardName: '',
+          cardNumber: '',
+          cardMonth: '',
+          cardYear: '',
+          cardCvv: ''
+        }
+      }
+    },
+    methods: {
+      updateCardNumber (val) {
+      },
+      updateCardName (val) {
+      },
+      updateCardMonth (val) {
+      },
+      updateCardYear (val) {
+      },
+      updateCardCvv (val) {
+      }
+    },
+  }
 
 </script>
 
 <style scoped >
 
-    .bcg {
-        background-color: #f4f4f5;
-        width: 100%;
-        height: 100%;
-    }
+  .bcg {
+    background-color: #f4f4f5;
+    width: 100%;
+    height: 100%;
+  }
 </style>
